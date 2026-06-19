4a9f335ae3242eb8eeee23fe0b8cafe018c794c0
Sudhan D <dsudhan.dev@gmail.com>
2026-06-19 12:00:09 +0530

    new character star pattern

--- star_patterns/star20.java
+++ star_patterns/star20.java
@@ -1,18 +1,9 @@
 import java.util.Scanner;
-public class star19 {
+public class star20 {
     public static void main(String[] args) {
        Scanner sc= new Scanner(System.in);
        int n=sc.nextInt();
-       for(int i=0;i<n;i++){
-        for(int j=0;j<(n)*2;j++){
-            if(j<n-i || j>=((n-i)+(i*2)) ){
-            System.out.print("*");}
-            else{
-                System.out.print(" ");
-            }
-        }System.out.println();
-        
-       }
+       
        for(int i=n-1;i>=0;i--){
         for(int j=0;j<n*2;j++){
             if(j<n-i || j>=((n-i)+(i*2)) ){
@@ -23,18 +14,28 @@ public class star19 {
         }System.out.println();
         
        }
+       for(int i=1;i<n;i++){
+        for(int j=0;j<(n)*2;j++){
+            if(j<n-i || j>=((n-i)+(i*2)) ){
+            System.out.print("*");}
+            else{
+                System.out.print(" ");
+            }
+        }System.out.println();
+        
+       }
     }
 }  
+
 /*
-**********
-****  ****
-***    ***
-**      **
-*        *
 *        *
 **      **
 ***    ***
 ****  ****
 **********
+****  ****
+***    ***
+**      **
+*        *
 
 */
