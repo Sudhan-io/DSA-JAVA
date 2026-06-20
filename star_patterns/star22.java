df46e45c1cda66cf92c73e161f31a44a869d535e
Sudhan D <dsudhan.dev@gmail.com>
2026-06-20 20:09:12 +0530

    new character star pattern

--- star_patterns/star22.java
+++ star_patterns/star22.java
@@ -3,13 +3,13 @@ public class star22 {
     public static void main(String[] args) {
        Scanner sc= new Scanner(System.in);
        int n=sc.nextInt();
-       for(int i=n*2-1;i>=1;i--){
-        for(int j=n*2-1;j>=1;j--){
-            if((i==n || i==1) || (j==n || j==1)){
-                System.out.print(i);
-            }else{
-                System.out.print("");
-            }
+       for(int i=0;i<n*1;i++){
+        for(int j=0;j<n*2-1;j++){
+            int top=i;
+            int bottom=j;
+            int left=(2*n-2)-i;
+            int right=(2*n-2)-j;
+            
         }
         System.out.println();
        } 
