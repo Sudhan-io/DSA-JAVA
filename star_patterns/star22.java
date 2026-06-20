4c8ea322d49a66deca7beb6e09f67baf1d117cef
Sudhan D <dsudhan.dev@gmail.com>
2026-06-20 22:31:24 +0530

    new numerical concentric square pattern

--- star_patterns/star22.java
+++ star_patterns/star22.java
@@ -1,18 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 public class star22 {
     public static void main(String[] args) {
        Scanner sc= new Scanner(System.in);
        int n=sc.nextInt();
-       for(int i=0;i<n*1;i++){
+       for(int i=0;i<2*n-1;i++){
         for(int j=0;j<n*2-1;j++){
             int top=i;
             int bottom=j;
             int left=(2*n-2)-i;
             int right=(2*n-2)-j;
-            
+            int min=Math.min(Math.min(top,bottom),Math.min(left,right));
+            System.out.print(n-min);
         }
         System.out.println();
        } 
     }
     
 }
+/*
+4444444
+4333334
+4322234
+4321234
+4322234
+4333334
+4444444
+
+*/
