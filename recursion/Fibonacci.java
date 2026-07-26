48e8b3ec6f2ff31149ad85490d57de00c7ecde1f
Sudhan D <dsudhan.dev@gmail.com>
2026-07-26 13:21:01 +0530

    new file and some configurations

---
+++ recursion/Fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        sc.close();
+    }
+
+    public static void solve() {
+        
+    }
+}
